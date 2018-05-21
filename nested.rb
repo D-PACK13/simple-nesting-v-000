6253c987b0379c4b810e74ebd5e1521173d68ce6
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-21 15:16:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
         }
      }
      
-programmer_hash[:yukihiro_matsumoto] = {known_for: "Unix", languages: "C"}
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["Lisp", "C"]}
+programmer_hash
 
 end
 
